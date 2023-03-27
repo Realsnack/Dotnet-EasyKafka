bf9e2f908ac03fa6161b7d0deb76d789338c0528
Michal Svačina <msvacina@outlook.com>
2023-03-27 22:42:55 +0200

    Added example using ServiceExtensions

--- EasyKafkaExample/Program.cs
+++ EasyKafkaExample/Program.cs
@@ -1,3 +1,4 @@
+using EasyKafka.Extensions;
 using EasyKafka.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Two ways to add the producer service
+builder.Services.AddEasyKafkaProducer<string>(producerName: "OtherProducer");
 builder.Services.AddSingleton<ProducerService<string>>(provider =>
 {
     var logger = provider.GetRequiredService<ILogger<ProducerService<string>>>();
